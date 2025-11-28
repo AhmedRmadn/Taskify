66961dfbd145bc6eed54d1be61f465627d188ff8
Ahmed Ramadan <ahmednasser1902@gmail.com>
2025-11-28 16:07:35 +0200

    adding database config

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'mydb',
+  process.env.DB_USER || 'root',
+  process.env.DB_PASS || 'root',
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+    dialect: 'mysql',
+    logging: (msg) => console.log(msg) 
+  }
+);
+module.exports = sequelize;
