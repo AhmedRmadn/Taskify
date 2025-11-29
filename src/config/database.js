40831bf4d82f3121e4311bda152b6c1ec574200d
Ahmed Ramadan <ahmednasser1902@gmail.com>
2025-11-29 15:29:32 +0200

    fix database

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'mydb',
   process.env.DB_USER || 'root',
-  process.env.DB_PASS || 'root',
+  process.env.DB_PASSWORD || 'password',
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
